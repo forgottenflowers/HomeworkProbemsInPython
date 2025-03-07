7cdb8f4a221a7f1051e0da00d9af939a8137a536
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-07 14:03:43 +0100

    Create GuessNumber.py

---
+++ Basics/FunExercises/GuessNumber.py
@@ -0,0 +1,7 @@
+def Lowkhyaa():
+	x=int(raw_input("what is Lowkhyaa's roll number?"))
+	while  x!=20091068:
+		x=int(raw_input("oh that's wrong! try again...."))
+	print "you are absolutely right! you are a genius!"
+
+Lowkhyaa()
