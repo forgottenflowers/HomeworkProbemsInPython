e66fc50800d76b3c69af2ad163e805e39aa7b835
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-07 14:20:40 +0100

    Create Sort.py

---
+++ Basics/Sort.py
@@ -0,0 +1,14 @@
+def s(seq):
+	for i in range(0,len(seq)):
+
+  		for j in range(i+1,len(seq)):
+
+   			if seq[i]>seq[j]:
+
+    				seq[i],seq[j]=seq[j],seq[i]
+	print seq
+
+
+
+
+s([6,3,8,4,1,6,67,35])
