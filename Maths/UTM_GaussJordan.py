ac2453aabe066c52f05b26b734b1ec0fefde3147
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-07 16:52:30 +0100

    Update UTM_GaussJordan.py

--- Maths/UTM_GaussJordan.py
+++ Maths/UTM_GaussJordan.py
@@ -7,35 +7,23 @@ def Gauss():
 	print "Enter the elements of the matrix",n,"X",n
 
 	x=[[0]*n for _ in [0]*n]
-
 	for s in range(0,n):
 
 		for w in range(0,n):
-
 			x[s][w]=int(raw_input("Enter x"+str(s+1)+str(w+1)+": "))
 
-
-
 	for s in range(0,n):
-
 		temp1=x[s][s]
 
 		for w in range(0,n):
-
 			x[s][w]=(1.0*x[s][w])/temp1
 
 		for w in range(0,n):
-
 			if s!=w:
-
 				temp2=x[w][s]
-
 				for q in range(s,n):
-
 					x[w][q]-=temp2*x[s][q]
 
 	print x
 
-
 Gauss()					
-	
