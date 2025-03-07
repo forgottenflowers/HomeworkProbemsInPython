8a814f5ac4af6f97022214424556628728320d70
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-07 16:50:59 +0100

    Update Inverse_Gauss.py

--- Maths/Inverse_Gauss.py
+++ Maths/Inverse_Gauss.py
@@ -13,15 +13,12 @@ def Gauss():
 	y=[]
 	z=[[0]*n for _ in [0]*n]
 	
-
 	for s in range(0,n):
 		temp1=x[s][s]
 		y.append(temp1)
 		for w in range(0,n):
 			x[s][w]=(1.0*x[s][w])/temp1
-			
-
-	
+				
 		for w in range(0,n):
 			if s!=w:
 				temp2=x[w][s]
@@ -29,23 +26,17 @@ def Gauss():
 				for q in range(s,n):
 					x[w][q]-=temp2*x[s][q]
 
-
-
 	for s in range(0,n):
 		
 		for w in range(0,n):
 			x[s][w]=(1.0*x[s][w])/y[s]
-
-    
-	
+    	
 		for w in range(0,n):
 			if s!=w:
 				
 				for q in range(0,n):
 					x[w][q]-=z[w][s]*x[s][q]
-					
-	
+						
 	print x
 	
-
 Gauss()					
