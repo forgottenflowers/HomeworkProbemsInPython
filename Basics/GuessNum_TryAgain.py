dc55dffb8c71217d5744e83a6fd260fc5093b2c5
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-09 08:33:40 +0100

    Update and rename GuessNum_choicetostop.py to GuessNum_TryAgain.py

--- Basics/GuessNum_TryAgain.py
+++ Basics/GuessNum_TryAgain.py
@@ -1,18 +1,17 @@
+# User is asked to guess a randomly generated number.
+# User is given the option to try again or quit.
+
 def guess():
 	import random
 
 	n=1
-
 	x=random.randrange(100)
-
 	t=1
-
 	y=int(raw_input("Guess the number....its between 0 and 100 "))
 
 	while n==1:
 		if y==x:
-			print "Congratulations! You are right! "
- 
+			print "Congratulations! You are right! " 
 			break
  
 		elif y<x: 
@@ -23,22 +22,17 @@ def guess():
 
 		op=raw_input("Do you want to try again? y/n ")
  
-		if op=="y":
- 
+		if op=="y": 
   			t=t+1 
-  
  			y=int(raw_input("Okay...so what's your guess? "))
-   
-
-   			continue
+      			continue
  
 		elif op=="n":
  			break
 
 		else: 
    
-			print "error!goodbye.."
-  
+			print "error!goodbye.."  
 			break
 
 	print "You have made "+str(t)+" trials" 
